7eac741d39bd49b46384c435d7f37464dc616266
iroatu08 <iroatu08@gmail.com>
2022-06-15 16:00:02 +0100

    setenv, unsetenv

--- shell.c
+++ shell.c
@@ -1,47 +1,73 @@
 #include "shell.h"
-/**
- * shell - Infinite loop that runs shell
- * @ac: Arg count
- * @av: args passed to shell at beginning of prog
- * @env: Environment
- * Return: Void
- */
-void shell(int ac, char **av, char **env)
-{
-	char *line;
-	char **args;
-	int status = 1;
-	char *tmp = NULL;
-	char *er;
-	char *filename;
-	int flow;
 
-	er = "Error";
-	do {
-		prompt();
-		line = _getline();
-		args = split_line(line);
-		flow = bridge(args[0], args);
-		if (flow == 2)
-		{
-			filename = args[0];
-			args[0] = find_path(args[0], tmp, er);
-			if (args[0] == er)
-			{
-				args[0] = search_cwd(filename, er);
-				if (args[0] == filename)
-					write(1, er, 5);
-			}
-		}
-		if (args[0] != er)
-			status = execute_prog(args, line, env, flow);
-		free(line);
-		free(args);
-	} while (status);
-	if (!ac)
-		(void)ac;
-	if (!av)
-		(void)av;
-	if (!env)
-		(void)env;
+/**
+ * free_data - frees data structure
+ *
+ * @datash: data structure
+ * Return: no return
+ */
+void free_data(data_shell *datash)
+{
+	unsigned int i;
+
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		free(datash->_environ[i]);
+	}
+
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * set_data - Initialize data structure
+ *
+ * @datash: data structure
+ * @av: argument vector
+ * Return: no return
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
+
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
 }
